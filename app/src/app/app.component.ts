e3ec02553da7c7e40e74e938f1516b77c45a5180
Amir Zare Pashaei <amir.zare.pashai@gmail.com>
2020-04-16 15:52:28 +0300

    changing the header to get inital value and adding changedetecor on push

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Chuck Norris Jokes';
 }
